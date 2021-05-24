a8851b15f31516901931d6c77677793d8c9515d8
Cherish Truong <groovu@gmail.com>
2021-05-24 10:42:17 -0700

    test commit from VS

---
+++ Mod5Demo/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mod5Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Price = 4;
+            ViewBag.names = new List<string>() { "deepali", "name1", "name2" };
+            return View();
+        }
+    }
+}
---
+++ Mod5Demo/Models/Format.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mod5Demo.Models
+{
+    public interface IFormat
+    {
+        string GetFormattedNumber(int number);
+    }
+    public class FormatNumber : IFormat
+    {
+        public string GetFormattedNumber(int number)
+        {
+            return string.Format("{0:n0}", number);
+        }
+    }
+}
